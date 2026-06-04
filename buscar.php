d01734a459b9c70deb58dc62d73d188385966292
iabraham120 <325300944@alumno.enp.unam.mx>
2026-06-04 14:34:20 -0600

    vrga de monooooo

--- buscar.php
+++ buscar.php
@@ -1,5 +1,40 @@
 <?php
+// Archivo: buscar.php
+include 'config.php';
 
+$texto_buscado = "";
+$lista_resultados = array();
+
+if (isset($_GET['busqueda'])) {
+    $texto_buscado = $_GET['busqueda'];
+    
+    $sql = "SELECT set_num, name, year, num_parts, theme_id
+            FROM sets 
+            WHERE name LIKE '%" . $texto_buscado . "%'";
+
+    $resultado_query = mysqli_query($conexion, $sql);
+    
+    if ($resultado_query) {
+        while ($fila = mysqli_fetch_assoc($resultado_query)) {
+            $theme_id = $fila["theme_id"];
+            
+            $sql2 = "SELECT name FROM themes WHERE theme_id = $theme_id";
+            $query2 = mysqli_query($conexion, $sql2);
+            
+            if ($query2) {
+                $res = mysqli_fetch_assoc($query2);
+                
+                if ($res) {
+                    $fila["theme_name"] = $res["name"];
+                } else {
+                    $fila["theme_name"] = "Sin tema";
+                }
+            }
+            
+            $lista_resultados[] = $fila;
+        }
+    }
+}
 ?>
 
 <!DOCTYPE html>
